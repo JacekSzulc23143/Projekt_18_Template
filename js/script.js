7a60cb6dd26b660e4ec3c1fcdd507ae6ef1fa2b1
Jacek Szulc <j.szulc77@wp.pl>
2024-11-02 21:11:14 +0100

    Projekt#18.2

--- js/script.js
+++ js/script.js
@@ -5,16 +5,28 @@ const squareTemp = document.querySelector(".square-temp");
 let num = 1;
 
 const addMoreSquares = () => {
-	const square = document.createElement("div");
-	square.classList.add("square");
+	const square = squareTemp.content.cloneNode(true);
 
-	square.innerHTML = `
-        <div class="square">
-            <p class="square-number">${num}</p>
-        </div>`;
+	square.querySelector(".square-number").textContent = num;
+	// square.querySelector("h1").textContent = "😍";
 
 	wrapper.appendChild(square);
+
 	num++;
 };
 
 btn.addEventListener("click", addMoreSquares);
+
+
+// const addMoreSquares = () => {
+// 	const square = document.createElement("div");
+// 	square.classList.add("square");
+
+// 	square.innerHTML = `
+//         <div class="square">
+//             <p class="square-number">${num}</p>
+//         </div>`;
+		
+// 	wrapper.appendChild(square);
+// 	num++;
+// };
